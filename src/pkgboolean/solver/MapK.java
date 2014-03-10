2d5e8ac1a7faf542bcf398e07bf89d4cd7baa1de
Edilson Gonzalez <edilson.gonza@unitec.edu>
2014-03-10 13:18:51 -0600

    Agregando MapaK al repositorio

---
+++ src/pkgboolean/solver/MapK.java
@@ -0,0 +1,26 @@
+
+package pkgboolean.solver;
+
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Edilson
+ */
+public class MapK {
+    
+    private TruthTable mainTable;
+    
+    public MapK(TruthTable table) {
+        this.mainTable = table;
+    }
+    
+    public Function simplifyaFunction() {
+        
+        return null;
+    }
+    
+    public TruthTable getTable() {
+        return this.mainTable;
+    }
+}
