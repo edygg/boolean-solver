df90669f2b44230515520cd75d5b3030de73fa35
Edilson Gonzalez <edilson.gonza@unitec.edu>
2014-03-15 10:31:04 -0600

    Nomenclatura SUM y PI agregada en Function

--- src/pkgboolean/solver/Function.java
+++ src/pkgboolean/solver/Function.java
@@ -2,6 +2,7 @@ package pkgboolean.solver;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.TreeSet;
 
 /**
  *
@@ -9,6 +10,8 @@ import java.util.Collections;
  */
 public class Function {
 
+    public static final String sopPattern = "([a-zA-z][\\']{0,1})*([\\+]([a-zA-z][\\']{0,1})*)*";
+    public static final String dontCarePattern = "([s|p][\\(]([0-9]|[1-2][0-9]|[3][0-1])([\\,][0-9]|[\\,][1-2][0-9]|[\\,][3][0-1]){0,30}[\\)])([\\+][d][\\(]([0-9]|[1-2][0-9]|[3][0-1])([\\,][0-9]|[\\,][1-2][0-9]|[\\,][3][0-1]){0,30}[\\)]){0,1}";
     private ArrayList<Variable> listVariables;
     private String function;
 
@@ -20,25 +23,89 @@ public class Function {
             throw new InvalidDataException("Function not write in the correct format");
         }
 
-        this.identifyVariables();
+        if (this.function.matches(sopPattern)) {
+            this.identifyVariables();
+        } else {
+            if (!this.parseCompactFunction()) {
+                throw new InvalidDataException("Please check the function again");
+            }
+        }
         Collections.sort(this.listVariables);
     }
 
     private boolean validate() {
-        String sopPattern = "([a-zA-z][\\']{0,1}[\\+]{0,1})*";
         //SOP
         if (this.function.matches(sopPattern)) {
             return true;
         }
-        
-        String dontCarePattern = "([s|p|d][(]([0-31][\\\\,]{0,1})*[)])";
+
         //Dont Care
         if (this.function.matches(dontCarePattern)) {
             return true;
         }
-        
+
         this.function = "";
         return false;
+    }
+
+    private boolean parseCompactFunction() throws InvalidDataException {
+        String[] parts = this.function.split("[+]");
+        TreeSet<Integer> termsSet = new TreeSet();
+
+        parts[0] = parts[0].replace("s(", "");
+        parts[0] = parts[0].replace("p(", "");
+        parts[0] = parts[0].replace(")", "");
+
+        String terms[] = parts[0].split(",");
+
+        for (int i = 0; i < terms.length; i++) {
+            if (!termsSet.contains(Integer.parseInt(terms[i]))) {
+                termsSet.add(Integer.parseInt(terms[i]));
+            } else {
+                return false;
+            }
+        }
+        
+        if (parts.length == 2) {
+            parts[1] = parts[1].replace("d(", "");
+            parts[1] = parts[1].replace(")", "");
+            
+            String dontCareterms[] = parts[1].split(",");
+
+            for (int i = 0; i < dontCareterms.length; i++) {
+                if (!termsSet.contains(Integer.parseInt(dontCareterms[i]))) {
+                    termsSet.add(Integer.parseInt(dontCareterms[i]));
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        int highestTerm = termsSet.last();
+        int iterations = 1;
+        if (highestTerm < 2) {
+            iterations = 1;
+        } else if (highestTerm < 4) {
+            iterations = 2;
+        } else if (highestTerm < 8) {
+            iterations = 3;
+        } else if (highestTerm < 16) {
+            iterations = 4;
+        } else if (highestTerm < 32) {
+            iterations = 5;
+        } else {
+            //No se pueden mas de 5 variables para esta nomenclatura
+            return false;
+        }
+
+        int variableChar = 65;
+
+        for (int i = 0; i < iterations; i++) {
+            this.listVariables.add(new Variable((char) variableChar, false));
+            variableChar++;
+        }
+
+        return true;
     }
 
     private void identifyVariables() throws InvalidDataException {
